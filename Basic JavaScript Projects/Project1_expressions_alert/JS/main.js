5057578b15921ece883f10ade2e3a435a2ca8d74
Nicolas Sloan <NicolasESloan@gmail.com>
2023-07-19 23:33:38 -0700

    First practice using JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello World!");
+document.write("Hello, world!" + "This is a concatenated message! ");
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string.";
+document.write(Sent1 + Sent2); //Concatenate both variables together under one function
+
+var y =" This is a test using \"quotation marks\"";
+document.write(y);
+
+Z=3+3; //expresion
+document.write(Z)
